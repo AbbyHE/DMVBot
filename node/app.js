411e70e02a3a7927f477d8d180278187ccc4f71c
Xu He <abbyhe@fb.com>
2016-09-14 13:44:04 -0700

    start server at port 3000

--- node/app.js
+++ node/app.js
@@ -1,11 +1,19 @@
 var express = require('express');
 var app = express();
 
+app.get('/', function(req,res) {
+    res.send('Hello world');
+});
+
 app.get('/webhook', function(req, res) {
   if (req.query['hub.mode'] === 'subscribe' &&
-      req.query['hub.verify_token'] === VALIDATION_TOKEN) {
+      req.query['hub.verify_token'] === 'dmvbot') {
     res.status(200).send(req.query['hub.challenge']);
   } else {
     res.sendStatus(403);
   }
+});
+
+var server = app.listen(3000, function() {
+    console.log("We have started our server on port 3000");
 });
