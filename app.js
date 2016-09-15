c29a9833d0f713ccdf4c67c39ade1c9f7bca8ea2
Xu He <abbyhe@fb.com>
2016-09-14 22:16:36 -0700

    add some constants

--- app.js
+++ app.js
@@ -5,13 +5,34 @@ var app = express();
 
 app.set('port', (process.env.PORT || 5000));
 
+/*
+ * Some constants
+ *
+ */
+const VALIDATION_TOKEN = process.env.VALIDATION_TOKEN
+  ? process.env.VALIDATION_TOKEN
+  : config.get('validationToken');
+
+const PAGE_ACCESS_TOKEN = process.env.PAGE_ACCESS_TOKEN
+  ? process.env.PAGE_ACCESS_TOKEN
+  : config.get('pageAccessToken');
+
+/*
+ * Kitty welcome screen
+ *
+ */
 app.get('/', function(req, res) {
   res.sendFile(__dirname + '/assets/coffee_cat.jpg');
 });
 
+/*
+ * Wehbook verification
+ *
+ */
 app.get('/webhook', function(req, res) {
+  console.log(VALIDATION_TOKEN);
   if (req.query['hub.mode'] === 'subscribe' &&
-      req.query['hub.verify_token'] === config.get('validationToken')) {
+      req.query['hub.verify_token'] === VALIDATION_TOKEN) {
     res.status(200).send(req.query['hub.challenge']);
   } else {
     res.sendStatus(403);
