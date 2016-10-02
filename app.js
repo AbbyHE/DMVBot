6a3fc659c5138cc0366d4a1858381b487a597ff9
Xu He <abbyhe@fb.com>
2016-10-01 23:01:03 -0700

    add callback verification

--- app.js
+++ app.js
@@ -10,7 +10,7 @@ var
 
 app.set('port', (process.env.PORT || 5000));
 
-app.use(bodyParser.json());
+app.use(bodyParser.json({ verify: verifyRequestSignature }));
 
 /*
  * Some constants
@@ -225,6 +225,31 @@ function callSendAPI(messageData) {
       console.error(res.error);
     }
   });
+}
+
+/*
+ * Verify that the callback came from Facebook.
+ *
+ */
+function verifyRequestSignature(req, res, buf) {
+  var signature = req.headers["x-hub-signature"];
+
+  if (!signature) {
+    console.error("Couldn't validate the signature.");
+    throw new Error("Couldn't validate the signature.");
+  } else {
+    var elements = signature.split('=');
+    var method = elements[0];
+    var signatureHash = elements[1];
+
+    var expectedHash = crypto.createHmac('sha1', APP_SECRET)
+                        .update(buf)
+                        .digest('hex');
+
+    if (signatureHash != expectedHash) {
+      throw new Error("Couldn't validate the request signature.");
+    }
+  }
 }
 
 app.listen(app.get('port'), function() {
